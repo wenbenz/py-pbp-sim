c6c8401468fc2b530734a75187cb35855a5975fd
Ben Zhao <ben@wenbinzhao.com>
2020-04-08 22:58:06 -0400

    feed training data back into network

--- Sim.py
+++ Sim.py
@@ -7,7 +7,7 @@ Evaluates perceptron branch predictor on given an input file path and N, the num
 The input file should be as follows:
     - each line represents a unique tuple of (address, taken/not taken)
     - each line should contain 2 numbers separated by a comma
-    - taken and not taken should be represented as 1 and -1 respectively
+    - taken and not taken should be represented as 1 and 0 respectively
     - example test.txt
 """
 if __name__ == '__main__':
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     N = int(sys.argv[2])
 
     p = Predictor(N)
+    p_taken, p_not_taken = 0, 0
     hits = 0
     total = 0
     f = open(filepath, "r")
@@ -24,8 +25,15 @@ if __name__ == '__main__':
     for l in lines:
         total += 1
         addr, x = l.split(",")
-        addr, x = int(addr) % 10, int(x)
+        addr, x = int(addr) % 10, int(x) * 2 - 1
         y = p.predict(addr)
+        if y == 1:
+            p_taken += 1
+        else:
+            p_not_taken += 1
         if x == y:
             hits += 1
-    print(hits / total)
+        p.train(addr, x)
+    print("accuracy:", hits / total)
+    print("predicted taken:", p_taken)
+    print("predicted not taken:", p_not_taken)
